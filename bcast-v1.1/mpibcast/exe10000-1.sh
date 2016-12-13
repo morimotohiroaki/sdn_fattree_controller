d61df158e6545392fd40a1e17101363c40804954
root <root@vm5.rootlinks.net>
2016-12-13 14:27:40 +0900

    add exe_file after compiling

---
+++ bcast-v1.1/mpibcast/exe10000-1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 0 10 10000 0 0
--- bcast-v1.1/mpibcast/exe5000.sh
+++ bcast-v1.1/mpibcast/exe5000.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 1 5000 0 0
-mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 1 5000 0 0
-mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 1 5000 0 0
-mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 1 5000 0 0
-mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 1 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
+mpirun -np 4 --mca btl_tcp_if_include ens3 -machinefile host2 ./test host2 1 10 5000 0 0
